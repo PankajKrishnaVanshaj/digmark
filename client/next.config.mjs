3f0f2aa820823a8311c123f093c11fb7674c57a8
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-04 22:54:08 +0530

    pwa add

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,30 +1,22 @@
-import withPWA from 'next-pwa';
+
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "server.digmark.pankri.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "55555", 
-        pathname: "/**",
-      },
-    ],
-  },
-  
+  const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "server.digmark.pankri.com",
+          pathname: "/**",
+        },
+        {
+          protocol: "http",
+          hostname: "localhost",
+          port: "55555", 
+          pathname: "/**",
+        },
+      ],
+    },
 };
 
-// Enable PWA
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true, // Automatically update service workers
-})(nextConfig);
-
-export default pwaConfig;
+export default nextConfig;
