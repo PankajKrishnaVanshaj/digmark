0eccf31a8e67d23324c57c2e9ed93621b11cca0f
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-01-13 13:22:37 +0530

    creator navigation to book page

--- client/src/app/(pages)/[creator]/page.tsx
+++ client/src/app/(pages)/[creator]/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client"
+import { useParams, useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 const Creator = () => {
+  const { creator } = useParams(); // Destructures the 'creator' param from the route
+  const router = useRouter(); // Access the Next.js router
+
+  useEffect(() => {
+    // Automatically navigate to 'creator/book' when the component mounts
+    router.push(`/${creator}/book`);
+  }, [router]);
+
   return (
     <div>Creator</div>
-  )
-}
+  );
+};
 
-export default Creator
+export default Creator;
